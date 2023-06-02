95621498af22f105d8abe8424576e52dd6ca90fc
Deven Phillips <josphill@redhat.com>
2023-06-02 09:40:01 -0400

    Fix context path

--- src/.vuepress/config-staging.js
+++ src/.vuepress/config-staging.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'Cloud-Native AppDev',
-  base: "/redhat-appdev-practice-website/",
+  base: "/website/",
   description: 'Cloud-Native Application Development tutorials, tricks, tips, and methods.',
   plugins: [
     'tabs',
